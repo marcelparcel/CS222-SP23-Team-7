605b46ef09ac2397285da143168eb4b697614e36
JuanDiegoSanz7 <67335772+JuanDiegoSanz7@users.noreply.github.com>
2023-02-27 19:09:44 -0600

    Create app.py
    yes

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify #import libraries
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def get_articles():
+    return jsonify({"Hello" : "World"})
+
+if __name__ == "__main__":
+    app.run(debug = True)
